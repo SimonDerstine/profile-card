69b419ed44030d6a6e0d1af388071e8382f06791
Simon Derstine <Simon Derstine>
2025-11-15 14:24:24 -0500

    Add movie poster and updated button

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ const goalText = document.getElementById("goal-text");
 
 btn.addEventListener("click", () => {
   goalText.classList.toggle("hidden");
+  const poster = document.getElementById("poster");
 });
 
 const nameE1 = document.getElementById("name");
 
-nameE1.addEventListener("click", () => {
-    nameE1.style.color = nameE1.style.color === "red" ? "blue" : "red" ;
+btn.addEventListener("click", () => {
+    goalText.classList.toggle("hidden");
+    poster.classList.toggle("hidden");
 });
+
 
